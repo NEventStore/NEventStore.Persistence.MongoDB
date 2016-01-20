4fd6b2c1caf85800eec5808443ed87c9bb683ddb
alkampfer <alkampfer@nablasoft.com>
2016-01-20 17:35:31 +0100

    Modified script for versioning.

--- build/Modules/BuildFunctions.psm1
+++ build/Modules/BuildFunctions.psm1
@@ -1,3 +1,46 @@
+function Update-SourceVersion
+{
+  Param 
+  (
+    [string]$SrcPath,
+    [string]$filePattern = 'AssemblyInfo.cs',
+    [string]$assemblyVersion, 
+    [string]$fileAssemblyVersion,
+    [string]$assemblyInformationalVersion
+  )
+    
+    if ($fileAssemblyVersion -eq "")
+    {
+        $fileAssemblyVersion = $assemblyVersion
+    }
+
+        
+    if ($assemblyInformationalVersion -eq "")
+    {
+        $assemblyInformationalVersion = $fileAssemblyVersion
+    }
+    
+    Write-Host "Executing Update-SourceVersion in path $SrcPath, Version is $assemblyVersion and File Version is $fileAssemblyVersion and Informational Version is $assemblyInformationalVersion"
+        
+    $AllVersionFiles = Get-ChildItem $SrcPath\* -Include $filePattern -recurse
+  
+    foreach ($file in $AllVersionFiles)
+    { 
+        Write-Host "Modifying file " + $file.FullName
+        #save the file for restore
+        $backFile = $file.FullName + "._ORI"
+        $tempFile = $file.FullName + ".tmp"
+        Copy-Item $file.FullName $backFile -Force
+        #now load all content of the original file and rewrite modified to the same file
+        Get-Content $file.FullName |
+        %{$_ -replace 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', "AssemblyVersion(""$assemblyVersion"")" } |
+        %{$_ -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', "AssemblyFileVersion(""$fileAssemblyVersion"")" } |
+        %{$_ -replace 'AssemblyInformationalVersion\(".*"\)', "AssemblyInformationalVersion(""$assemblyInformationalVersion"")" } > $tempFile
+        Move-Item $tempFile $file.FullName -Force
+    }
+ 
+}
+
 function Get-Version
 {
 	param
---
+++ build/version.ps1
@@ -0,0 +1,32 @@
+Write-Output "Starting versioning script"
+if(Get-Module -name BuildFunctions) 
+{
+    Remove-Module BuildFunctions
+}
+if(-not(Get-Module -name BuildFunctions)) 
+{
+    Import-Module -Name ".\Modules\BuildFunctions"
+}
+Write-Output "Invoking GitVersion.exe"
+$Output = & ..\bin\GitVersion\Gitversion.exe /nofetch | Out-String
+Write-Output ("GitVersion returned:  " + $output)
+
+
+$version = $output | ConvertFrom-Json
+$assemblyVersion = $version.AssemblySemver
+$assemblyFileVersion = $version.AssemblySemver
+$assemblyInformationalVersion = ($version.SemVer + "/" + $version.Sha)
+
+Write-Output "SemVer - Assembly and File: $assemblyVersion Informational: $assemblyInformationalVersion"
+
+Write-Output $version.NuGetVersionV2
+Write-Output ("##vso[task.setvariable variable=NugetVersion;]" + $version.NugetVersionV2)
+Write-Output ("##vso[task.setvariable variable=AssemblyVersion;]" + $assemblyVersion)
+Write-Output ("##vso[task.setvariable variable=FileInfoVersion;]" + $assemblyFileVersion)
+Write-Output ("##vso[task.setvariable variable=AssemblyInformationalVersion;]" + $assemblyInformationalVersion)
+
+#change build number.
+Write-Output ("##vso[task.setvariable variable=build.buildnumber;]" + $version.FullSemVer)
+Write-Output ("##vso[build.updatebuildnumber]" + $version.FullSemVer)
+
+Update-SourceVersion -SrcPath ..\src -FilePattern 'VersionAssemblyInfo.cs' -assemblyVersion $assemblyVersion -fileAssemblyVersion $assemblyFileVersion -assemblyInformationalVersion $assemblyInformationalVersion
