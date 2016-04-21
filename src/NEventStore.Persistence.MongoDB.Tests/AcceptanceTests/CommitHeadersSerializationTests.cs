83af5bc32350c522612e7aa8093eba90be00fd88
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2016-04-21 12:05:33 +0200

    Fixed the test.

--- src/NEventStore.Persistence.MongoDB.Tests/AcceptanceTests/CommitHeadersSerializationTests.cs
+++ src/NEventStore.Persistence.MongoDB.Tests/AcceptanceTests/CommitHeadersSerializationTests.cs
@@ -1,4 +1,6 @@
-﻿using NEventStore.Persistence.AcceptanceTests.BDD;
+﻿using MongoDB.Bson;
+using NEventStore.Persistence.AcceptanceTests;
+using NEventStore.Persistence.AcceptanceTests.BDD;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,38 +14,48 @@ namespace NEventStore.Persistence.MongoDB.Tests.AcceptanceTests
 	/// </summary>
 	public class when_serializing_headers_as_Document_and_a_commit_header_has_a_name_that_contains_a_period : SpecificationBase
 	{
-		private ICommit _persisted;
+		// private ICommit _persisted;
 		private string _streamId;
 		private IPersistStreams Persistence;
+
+		private Exception _thrown;
 
 		protected override void Context()
 		{
 			Persistence = new AcceptanceTestMongoPersistenceFactory(
-				new MongoPersistenceOptions {
-					CommitHeadersDictionaryRepresentation = 
-						global::MongoDB.Bson.Serialization.Options.DictionaryRepresentation.Document }
+				new MongoPersistenceOptions
+				{
+					CommitHeadersDictionaryRepresentation =
+						global::MongoDB.Bson.Serialization.Options.DictionaryRepresentation.Document
+				}
 				).Build();
-
-			_streamId = Guid.NewGuid().ToString();
-			var attempt = new CommitAttempt(_streamId,
-				2,
-				Guid.NewGuid(),
-				1,
-				DateTime.Now,
-				new Dictionary<string, object> { { "key.1", "value" } },
-				new List<EventMessage> { new EventMessage { Body = new NEventStore.Persistence.AcceptanceTests.ExtensionMethods.SomeDomainEvent { SomeProperty = "Test" } } });
-			Persistence.Commit(attempt);
 		}
 
 		protected override void Because()
 		{
-			_persisted = Persistence.GetFrom(_streamId, 0, int.MaxValue).First();
+			_thrown = Catch.Exception(() =>
+			{
+				_streamId = Guid.NewGuid().ToString();
+				var attempt = new CommitAttempt(_streamId,
+					2,
+					Guid.NewGuid(),
+					1,
+					DateTime.Now,
+					new Dictionary<string, object> { { "key.1", "value" } },
+					new List<EventMessage> { new EventMessage { Body = new NEventStore.Persistence.AcceptanceTests.ExtensionMethods.SomeDomainEvent { SomeProperty = "Test" } } });
+				Persistence.Commit(attempt);
+			});
+
+			// _persisted = Persistence.GetFrom(_streamId, 0, int.MaxValue).First();
 		}
 
 		[Fact]
-		public void should_correctly_deserialize_headers()
+		public void should_throw_serialization_exception_due_to_invalid_key()
 		{
-			_persisted.Headers.Keys.ShouldContain("key.1");
+			// _persisted.Headers.Keys.ShouldContain("key.1");
+
+			_thrown.ShouldBeInstanceOf<BsonSerializationException>();
+			_thrown.Message.ShouldContain("key.1");
 		}
 	}
 
@@ -95,9 +107,11 @@ namespace NEventStore.Persistence.MongoDB.Tests.AcceptanceTests
 		protected override void Context()
 		{
 			Persistence = new AcceptanceTestMongoPersistenceFactory(
-				new MongoPersistenceOptions {
-					CommitHeadersDictionaryRepresentation = 
-					global::MongoDB.Bson.Serialization.Options.DictionaryRepresentation.ArrayOfArrays }
+				new MongoPersistenceOptions
+				{
+					CommitHeadersDictionaryRepresentation =
+					global::MongoDB.Bson.Serialization.Options.DictionaryRepresentation.ArrayOfArrays
+				}
 				).Build();
 
 			_streamId = Guid.NewGuid().ToString();
