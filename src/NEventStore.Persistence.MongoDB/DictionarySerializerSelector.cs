3366fcbaf243da75f5c334bafb34f8ca5122f98c
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2016-04-21 11:39:49 +0200

    Added the ability to configure the DictionaryRepresentation for the Commit.Headers dictionary collection.

---
+++ src/NEventStore.Persistence.MongoDB/DictionarySerializerSelector.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Options;
+using MongoDB.Bson.Serialization.Serializers;
+using System.Collections.Generic;
+
+namespace NEventStore.Persistence.MongoDB
+{
+	/// <summary>
+	/// this is used in an extension method, so it should be passed as argument or cached in a static field
+	/// </summary>
+	internal static class DictionarySerializerSelector
+	{
+		internal static DictionaryRepresentation DictionaryRepresentation;
+		internal static IBsonSerializer DictionarySerializer;
+
+		public static void SetDictionaryRepresentation(DictionaryRepresentation dictionaryRepresentation)
+		{
+			DictionaryRepresentation = dictionaryRepresentation;
+			DictionarySerializer = new DictionaryInterfaceImplementerSerializer<Dictionary<string, object>>(DictionaryRepresentation);
+		}
+	}
+}
--- src/NEventStore.Persistence.MongoDB/MongoPersistenceOptions.cs
+++ src/NEventStore.Persistence.MongoDB/MongoPersistenceOptions.cs
@@ -6,12 +6,13 @@ namespace NEventStore.Persistence.MongoDB
 	using System;
 	using global::MongoDB.Driver;
 	using NEventStore.Serialization;
+	using global::MongoDB.Bson.Serialization.Options;
 
 	/// <summary>
 	/// Options for the MongoPersistence engine.
 	/// http://docs.mongodb.org/manual/core/write-concern/#write-concern
 	/// </summary>
-	public  class MongoPersistenceOptions
+	public class MongoPersistenceOptions
 	{
 		/// <summary>
 		/// Get the  <see href="http://docs.mongodb.org/manual/core/write-concern/#write-concern">WriteConcern</see> for the commit insert operation.
@@ -52,7 +53,7 @@ namespace NEventStore.Persistence.MongoDB
 			};
 		}
 
-	    /// <summary>
+		/// <summary>
 		/// Connects to NEvenstore Mongo database
 		/// </summary>
 		/// <param name="connectionString">Connection string</param>
@@ -64,36 +65,48 @@ namespace NEventStore.Persistence.MongoDB
 			return database;
 		}
 
-        /// <summary>
-        /// This is the instance of the Id Generator I want to use to 
-        /// generate checkpoint. 
-        /// </summary>
-	    public ICheckpointGenerator CheckpointGenerator { get; set; }
+		/// <summary>
+		/// Select your serialization scheme for Commit.Headers dictionaty:
+		/// 
+		/// defaults to: DictionaryRepresentation.ArrayOfArrays 
+		/// </summary>
+		public DictionaryRepresentation CommitHeadersDictionaryRepresentation
+		{
+			get { return DictionarySerializerSelector.DictionaryRepresentation; }
+			set { DictionarySerializerSelector.SetDictionaryRepresentation(value); }
+		}
 
-        public ConcurrencyExceptionStrategy ConcurrencyStrategy { get; set; }
+		/// <summary>
+		/// This is the instance of the Id Generator I want to use to 
+		/// generate checkpoint. 
+		/// </summary>
+		public ICheckpointGenerator CheckpointGenerator { get; set; }
 
-        public String SystemBucketName { get; set; }
+		public ConcurrencyExceptionStrategy ConcurrencyStrategy { get; set; }
 
-        public MongoPersistenceOptions()
-	    {
-            SystemBucketName = "system";
-            ConcurrencyStrategy = ConcurrencyExceptionStrategy.Continue; 
-	    }
+		public String SystemBucketName { get; set; }
+
+		public MongoPersistenceOptions()
+		{
+			SystemBucketName = "system";
+			ConcurrencyStrategy = ConcurrencyExceptionStrategy.Continue;
+			CommitHeadersDictionaryRepresentation = DictionaryRepresentation.ArrayOfArrays;
+		}
 	}
 
-    public enum ConcurrencyExceptionStrategy
-    {
-        /// <summary>
-        /// When a <see cref="ConcurrencyException"/> is thrown, simply continue
-        /// and ask to <see cref="ICheckpointGenerator"/> implementation new id.
-        /// </summary>
-        Continue = 0,
+	public enum ConcurrencyExceptionStrategy
+	{
+		/// <summary>
+		/// When a <see cref="ConcurrencyException"/> is thrown, simply continue
+		/// and ask to <see cref="ICheckpointGenerator"/> implementation new id.
+		/// </summary>
+		Continue = 0,
 
-        /// <summary>
-        /// When a <see cref="ConcurrencyException"/> is thrown, generate an empty
-        /// commit with current <see cref="LongCheckpoint"/>, then ask to 
-        /// <see cref="ICheckpointGenerator"/> implementation new id.
-        /// </summary>
-        FillHole = 1,
-    }
+		/// <summary>
+		/// When a <see cref="ConcurrencyException"/> is thrown, generate an empty
+		/// commit with current <see cref="LongCheckpoint"/>, then ask to 
+		/// <see cref="ICheckpointGenerator"/> implementation new id.
+		/// </summary>
+		FillHole = 1,
+	}
 }
