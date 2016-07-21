5ffdb139b42436e6f69b578063cc7f1b4fbf6f95
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2016-07-21 15:12:09 +0200

    updated NEventStore submodule

--- dependencies/RestorePackages.ps1
+++ dependencies/RestorePackages.ps1
@@ -2,5 +2,5 @@ $path = $(Split-Path -parent $MyInvocation.MyCommand.path)
 
 gci $path\NEventStore\src -Recurse "packages.config" |% {
 	"Restoring " + $_.FullName
-	& $path\NEventStore\src\.nuget\nuget.exe i $_.FullName -o $path\NEventStore\src\packages
+	& $path\NEventStore\src\.nuget\nuget.exe install $_.FullName -o $path\NEventStore\src\packages
 }
